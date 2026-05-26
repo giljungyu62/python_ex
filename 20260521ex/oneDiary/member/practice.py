0091f62c7e90c54b18561a4249ed4bada9ca2ec2
giljungyu62 <giljungyu62>
2026-05-26 13:53:59 +0900

    test

---
+++ 20260521ex/oneDiary/member/practice.py
@@ -0,0 +1,17 @@
+# '''
+# 1부터 입력받은 숫자 n까지 출력
+# '''
+# num = int(input('n :'))
+
+# i = 1
+# while i <= num:
+#     print(i)
+#     i = i + 1
+'''
+yes를 입력하면 반복하기
+'''
+character = "yes"
+while character == "yes":
+    character = input("yes 입력시 반복 ")
+
+print('종료')
